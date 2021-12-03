de7842c316616c7875ec427642eb4ff175790ea5
Mariam-Kabu <mariamkabulovi@gmail.com>
2021-12-03 23:23:54 +0400

    discovery page and responsive

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,18 @@
 // overlay menu - responsive
 const menuBar = document.querySelector('.toggle-bar');
 const overlayMenu = document.querySelector('.overlay-menu-container');
-const overlayLogo =document.querySelector('.toggleLogo');
+const overlayLogo = document.querySelector('.toggleLogo');
+const body = document.getElementsByTagName("BODY")
 let menuOpen = false;
-menuBar.addEventListener('click', ()=>{
-  if(!menuOpen){
+menuBar.addEventListener('click', () => {
+  if (!menuOpen) {
     overlayMenu.classList.add('open');
     overlayMenu.classList.remove('closed');
     menuOpen = true;
   }
 });
-overlayLogo.addEventListener('click', ()=>{
-  if(menuOpen){
+overlayLogo.addEventListener('click', function () {
+  if (menuOpen) {
     overlayMenu.classList.add('closed');
     overlayMenu.classList.remove('open');
     menuOpen = false;
